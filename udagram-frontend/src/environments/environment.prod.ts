2c488f051d73776a3eb4dc33c490c4d09ee5a7cb
emanelsayed39 <88773470+emanelsayed39@users.noreply.github.com>
2023-04-05 01:06:31 +0200

    try CI

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a1fded50c55634f37be75d4e44c7f884-1283899513.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
